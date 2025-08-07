c158734b31ecafcb8e6435b6a84a8974f84e664e
pipeline <pipeline@syncfusion.com>
2025-08-07 13:45:16 +0000

    v30.2.4 is released

--- src/pdfviewer/custom-toolbar.js
+++ src/pdfviewer/custom-toolbar.js
@@ -99,6 +99,7 @@ this.default = function () {
             { prefixIcon:"e-icons e-highlight-color", click: highlight.bind(this), tooltipText:"Highlight", id:"highlight", align:"Center" },
             { prefixIcon: 'e-icons e-underline', click: underLine.bind(this), tooltipText: 'Underline', id: 'underline', align: 'Center' },
             { prefixIcon: 'e-icons e-strikethrough', click: strikeThrough.bind(this), tooltipText: 'Strikethrough', id: 'strikethrough', align: 'Center' },
+            { prefixIcon: 'e-icons e-squiggly', click: squiggly.bind(this), tooltipText: 'Squiggly', id: 'squiggly', align: 'Center' },
             { type: 'Separator', tooltipText: 'separator', align: 'Center' },
             { prefixIcon: 'e-icons e-line', click: addLine.bind(this), tooltipText: 'Add Line', id: 'line', align: 'Center' },
             { prefixIcon: 'e-icons e-arrow-right-up', click: addArrow.bind(this), tooltipText: 'Add Arrow', id: 'arrow', align: 'Center' },
@@ -482,6 +483,11 @@ function openEditAnnotation(args) {
   function strikeThrough(e) {
     disableInkAnnotation();
     viewer.annotation.setAnnotationMode('Strikethrough');
+  }
+
+  function squiggly() {
+    disableInkAnnotation();
+    viewer.annotation.setAnnotationMode('Squiggly');
   }
   
   function addLine(e) {
